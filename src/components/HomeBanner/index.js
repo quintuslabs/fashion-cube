750292b53621d12bb2069212c2ed301cbbf901ab
Santosh Dash <santoshnet2016@gmail.com>
2020-04-10 16:34:35 +0530

    slider addded

--- src/components/HomeBanner/index.js
+++ src/components/HomeBanner/index.js
@@ -6,31 +6,81 @@
 
 import React from "react";
 
-import BackgroundImage from "../../assets/images/slider_1.jpg";
+import BackgroundImage1 from "../../assets/images/slider_1.jpg";
+import BackgroundImage2 from "../../assets/images/slider_2.jpg";
+import BackgroundImage3 from "../../assets/images/slider_3.jpg";
+import { Carousel } from "react-bootstrap";
 
 function HomeBanner(props) {
   return (
-    <div
-      className="main_slider"
-      style={{
-        backgroundImage: `url(${BackgroundImage})`
-      }}
-      data-aos="fade-up"
-    >
-      <div className="container fill_height">
-        <div className="row align-items-center fill_height">
-          <div className="col">
-            <div className="main_slider_content" data-aos="fade-right">
-              <h6>Spring / Summer Collection 2017</h6>
-              <h1>Get up to 30% Off New Arrivals</h1>
-              <div className="red_button shop_now_button">
-                <a href="#">shop now</a>
+    <Carousel>
+      <Carousel.Item>
+        <div
+          className="d-block w-100 main_slider"
+          style={{
+            backgroundImage: `url(${BackgroundImage1})`,
+          }}
+        >
+          <div className="container fill_height">
+            <div className="row align-items-center fill_height">
+              <div className="col">
+                <div className="main_slider_content" data-aos="fade-right">
+                  <h6>Spring / Summer Collection 2017</h6>
+                  <h1>Get up to 30% Off New Arrivals</h1>
+                  <div className="red_button shop_now_button">
+                    <a href="#">shop now</a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div
+          className="d-block w-100 main_slider"
+          style={{
+            backgroundImage: `url(${BackgroundImage2})`,
+          }}
+        >
+          <div className="container fill_height">
+            <div className="row align-items-center fill_height">
+              <div className="col">
+                <div className="main_slider_content" data-aos="fade-right">
+                  <h6>Spring / Summer Collection 2017</h6>
+                  <h1>Get up to 30% Off New Arrivals</h1>
+                  <div className="red_button shop_now_button">
+                    <a href="#">shop now</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div
+          className="d-block w-100 main_slider"
+          style={{
+            backgroundImage: `url(${BackgroundImage3})`,
+          }}
+        >
+          <div className="container fill_height">
+            <div className="row align-items-center fill_height">
+              <div className="col">
+                <div className="main_slider_content" data-aos="fade-right">
+                  <h6>Spring / Summer Collection 2017</h6>
+                  <h1>Get up to 30% Off New Arrivals</h1>
+                  <div className="red_button shop_now_button">
+                    <a href="#">shop now</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Carousel.Item>
+    </Carousel>
   );
 }
 

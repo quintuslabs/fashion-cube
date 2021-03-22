a673cafb48f7cbcf0a8976a53faccdc988d323a0
Santosh Kumar Dash <santoshnet2016@gmail.com>
2021-03-22 10:43:55 +0530

    Update mongo-config.js

--- server/ecommerce/configs/mongo-config.js
+++ server/ecommerce/configs/mongo-config.js
@@ -4,5 +4,4 @@ const MONGO_PASSWORD = 'your_password';
 const MONGO_HOSTNAME = '127.0.0.1';
 const MONGO_PORT = '27017';
 const MONGO_DB = 'sharkinfo';
-//module.exports = "mongodb://heroku_8bd94qrf:irstf0rv1ds970eebtislm0apf@ds029638.mlab.com:29638/heroku_8bd94qrf"
 module.exports = "mongodb://127.0.0.1:27017/fashion-cube"
